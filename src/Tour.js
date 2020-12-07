7f78ecab1c87208b773f48b9627454a1bd7a13b1
sumonsheikh <sm.sumon117@gmail.com>
2020-12-07 14:43:38 +0600

    add read more button to the paragraph

--- src/Tour.js
+++ src/Tour.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 const Tour = ({id,image,name,price,info}) => {
+    const [readMore, setReadMore] = useState(false);
     return (
         <article className="single-tour">
             <img src={image} alt={name}/>
@@ -8,7 +10,9 @@ const Tour = ({id,image,name,price,info}) => {
                     <h4>{name}</h4>
                     <h4 className="tour-price">{price}</h4>
                 </div>
-                <p>{info}</p>
+                <p>{readMore ? info : `${info.substring(0,200)}  `}
+                    <button onClick={()=>setReadMore(!readMore)}> read more </button>
+                </p>
                 <button className="delete">not interested</button>
             </footer>
         </article>
