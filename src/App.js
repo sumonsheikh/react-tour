0253366e9cd14bb7b4b4031a88e4172f271dc066
sumonsheikh <sm.sumon117@gmail.com>
2020-12-07 10:59:52 +0600

    component:tour,tours,loading created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
 import './App.css';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
   return (
-    <div className="App">
+    <div className = "App">
       
     </div>
   );
